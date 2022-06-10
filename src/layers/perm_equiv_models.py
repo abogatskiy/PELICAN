fdc67bd955b65dec19618581e54e558034a02929
level1807 <blackmail1807@gmail.com>
2022-06-09 22:18:42 -0400

    revert to only mean aggregator

--- src/layers/perm_equiv_models.py
+++ src/layers/perm_equiv_models.py
@@ -84,7 +84,7 @@ class Eq2to2(nn.Module):
         self.device = device
         self.dtype = dtype
         self.activation_fn = get_activation_fn(activation)
-        self.basis_dim = (7 if sym else 15) * 9
+        self.basis_dim = (7 if sym else 15) * 2
 
         self.out_dim = out_dim
         self.in_dim = in_dim
@@ -102,8 +102,9 @@ class Eq2to2(nn.Module):
     def forward(self, inputs, mask=None):
 
         nobj = mask[:,:,0].sum(1).squeeze()  # nobj.shape=[B]
-        ops0 = [self.ops_func(inputs, nobj, aggregation=agg) for agg in ['mean','max','min']]
-        ops1 = [y for x in ops0 for y in [x, x* ((1+nobj).log().view([-1,1,1,1,1]) / 3.845), x / ((1+nobj).log().view([-1,1,1,1,1]) / 3.845)]]
+        ops0 = [self.ops_func(inputs, nobj, aggregation='mean')]
+        # ops0 = [self.ops_func(inputs, nobj, aggregation=agg) for agg in ['mean','max','min']]
+        ops1 = [y for x in ops0 for y in [x, x* ((1+nobj).log().view([-1,1,1,1,1]) / 3.845)]] #, x / ((1+nobj).log().view([-1,1,1,1,1]) / 3.845)
         ops = torch.cat(ops1, dim=2)
         # ops = torch.cat(ops, dim=2)
 
