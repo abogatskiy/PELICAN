8eb7528906151209eec53072db0fdcae3891207c
level1807 <blackmail1807@gmail.com>
2022-06-17 14:23:33 -0400

    optuna copy script update

--- optuna_copydb.py
+++ optuna_copydb.py
@@ -15,15 +15,15 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--study-name', default='nosave')
-    parser.add_argument('--host', default='localhost')
+    parser.add_argument('--host', default='worker1172')
     parser.add_argument('--password', default='asoetuh')
-    parser.add_argument('--port', default='5432')
+    parser.add_argument('--port', default='35719')
 
     args = parser.parse_args()
     
     from_storage=f'postgresql://{os.environ["USER"]}:{args.password}@{args.host}:{args.port}'   # For running on nodes with a distributed file system
-    to_storage='sqlite:///file:'+args.study_name+'.db?vfs=unix-dotfile&uri=true'  # For running on a local machine
+    to_storage='sqlite:///file:'+args.study_name+'.db'  # For running on a local machine
 
-    study = optuna.copy_study(from_study_name=args.study_name, from_storage=storage, to_storage=to_storage, to_study_name=args.study_name)
+    study = optuna.copy_study(from_study_name=args.study_name, from_storage=from_storage, to_storage=to_storage, to_study_name=args.study_name)
   
     print(f"Study {args.study_name} successfully copied to {to_storage}: ")
