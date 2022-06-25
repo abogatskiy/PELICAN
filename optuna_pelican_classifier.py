8ab8dfc2b71fdff4c3dfbc0738baafe1f01939e1
level1807 <blackmail1807@gmail.com>
2022-06-24 23:46:54 -0400

    tweak optuna settings

--- optuna_pelican_classifier.py
+++ optuna_pelican_classifier.py
@@ -163,7 +163,7 @@ if __name__ == '__main__':
     if args.pruner == 'hyperband':
         pruner = optuna.pruners.HyperbandPruner()
     elif args.pruner == 'median':
-        pruner = optuna.pruners.MedianPruner()
+        pruner = optuna.pruners.MedianPruner(n_warmup_steps=10, n_min_trials=5)
 
     # pruner = optuna.pruners.MedianPruner(n_startup_trials=3, n_warmup_steps=14, n_min_trials=3)
     study = optuna.create_study(study_name=args.study_name, storage=storage, directions=directions, load_if_exists=True,
