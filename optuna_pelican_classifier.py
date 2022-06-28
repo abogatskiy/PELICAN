a237e90ea8e4a2efe6a9c64eea35004b4e61c3f9
level1807 <blackmail1807@gmail.com>
2022-06-28 10:37:48 -0400

    optuna script tweak

--- optuna_pelican_classifier.py
+++ optuna_pelican_classifier.py
@@ -29,12 +29,12 @@ def suggest_params(args, trial):
 
     args.batch_size = trial.suggest_categorical("batch_size", [8, 10, 16, 20, 32])
 
-    args.config = trial.suggest_categorical("config", ["s", "S", "m", "M", "sS", "mM", "sm", "sM", "Sm", "SM"]) #, "mx", "Mx", "sSm", "sSM", "smM", "sMmM", "mxn", "mXN", "mxMX", "sXN", "smxn"])
+    args.config = trial.suggest_categorical("config", ["s", "sm"]) #, "S", "m", "M", "sS", "mM", "sM", "Sm", "SM"]) #, "mx", "Mx", "sSm", "sSM", "smM", "sMmM", "mxn", "mXN", "mxMX", "sXN", "smxn"])
     
-    n_layers1 = trial.suggest_int("n_layers1", 4, 8)
-    n_layersm = [1,] * n_layers1
-    # n_layersm = [trial.suggest_int("n_layersm", 1, 2) for i in range(n_layers1)]
+    n_layers1 = 6 # trial.suggest_int("n_layers1", 6, 7)
 
+    # n_layersm = [1,] * n_layers1
+    n_layersm = [trial.suggest_int("n_layersm", 1, 2) for i in range(n_layers1)]
     args.num_channels_m = [[trial.suggest_int('n_channelsm['+str(i)+', '+str(k)+']', 5, 30) for k in range(n_layersm[i])] for i in range(n_layers1)]
     # args.num_channels_m = [[trial.suggest_int('n_channelsm['+str(k)+']', 10, 30) for k in range(n_layersm)]] * n_layers1
 
@@ -46,8 +46,8 @@ def suggest_params(args, trial):
     n_layers2 = 1
     args.num_channels2 = [trial.suggest_int("n_channels2["+str(i)+"]", 10, 30) for i in range(n_layers2)]
 
-    args.activation = trial.suggest_categorical("activation", ["relu", "elu", "leakyrelu", "silu", "selu", "tanh"])
-    args.optim = trial.suggest_categorical("optim", ["adamw", "sgd", "amsgrad", "rmsprop", "adam"])
+    args.activation = trial.suggest_categorical("activation", ["elu", "leakyrelu"]) #, "relu", "silu", "selu", "tanh"])
+    # args.optim = trial.suggest_categorical("optim", ["adamw", "sgd", "amsgrad", "rmsprop", "adam"])
 
     args.activate_agg = trial.suggest_categorical("activate_agg", [True, False])
     args.activate_lin = trial.suggest_categorical("activate_lin", [True, False])
@@ -179,41 +179,45 @@ if __name__ == '__main__':
 
     init_params =  {'activate_agg': False,
                     'activate_lin': True,
-                    'activation': 'elu',
-                    'batch_size': 10,
+                    'activation': 'leakyrelu',
+                    'batch_size': 20,
                     'config': 's',
                     'lr_final': 1e-07,
                     'lr_init': 0.001,
-                    'scale': 1.,
-                    'n_channels1[0]': 3,
-                    'n_channels1[1]': 15,
+                    'scale': 0.6,
+                    'n_channels1[0]': 25,
+                    'n_channels1[1]': 20,
                     'n_channels1[2]': 15,
                     'n_channels1[3]': 15,
                     'n_channels1[4]': 15,
-                    'n_channels1[5]': 15,
+                    'n_channels1[5]': 20,
+                    'n_channels1[5]': 25,
                     'n_channels2[0]': 30,
-                    'n_channelsm[0, 0]': 3,
+                    'n_channelsm[0, 0]': 30,
                     'n_channelsm[0, 1]': 15,                    
                     'n_channelsm[1, 0]': 15,
-                    'n_channelsm[1, 1]': 15,
+                    'n_channelsm[1, 1]': 30,
                     'n_channelsm[2, 0]': 15,
-                    'n_channelsm[2, 1]': 15,
+                    'n_channelsm[2, 1]': 30,
                     'n_channelsm[3, 0]': 15,
-                    'n_channelsm[3, 1]': 15,
+                    'n_channelsm[3, 1]': 30,
                     'n_channelsm[4, 0]': 15,
-                    'n_channelsm[4, 1]': 15,
-                    'n_layers1': 5,
-                    'n_layers2': 1,
+                    'n_channelsm[4, 1]': 30,
+                    'n_channelsm[5, 0]': 15,
+                    'n_channelsm[5, 1]': 30,
+                    # 'n_layers1': 6,
+                    # 'n_layers2': 1,
                     'n_layersm[0]': 2,
                     'n_layersm[1]': 2,
                     'n_layersm[2]': 2,
                     'n_layersm[3]': 2,
                     'n_layersm[4]': 2,
-                    'optim': 'adamw',
+                    'n_layersm[5]': 2,
+                    # 'optim': 'adamw',
                     }
     study.enqueue_trial(init_params)
                             
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective, callbacks=[optuna.study.MaxTrialsCallback(200, states=(TrialState.COMPLETE,))])
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
