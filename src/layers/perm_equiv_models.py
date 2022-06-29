try to fix NaNs coming out of softmax

src/layers/perm_equiv_models.py
@@ -172,7 +172,7 @@ class Net2to2(nn.Module):
             for layer, message, sig, normlayer in zip(self.eq_layers, self.message_layers, self.attention, self.normlayers):
                 x = message(x, mask)        # form messages at each of the NxN nodes
                 y = sig(x)                  # compute the dot product with the attention vector over the channel dim
-                yy = torch.exp(y - y.max()) * mask  # apply softmax over NxN particles taking into account the mask (normalized in the next line)
+                yy = torch.exp(y - y.amax(dim=(1,2), keepdim=True)) * mask  # apply softmax over NxN particles taking into account the mask (normalized in the next line)
                 yy = yy / yy.sum(dim=(1,2), keepdim=True)  
                 z = normlayer(yy * x)       # apply LayerNorm, i.e. normalize over the channel dimension
                 x = layer(z, mask, nobj)    # apply the permutation-equivariant layer
@@ -180,3 +180,4 @@ class Net2to2(nn.Module):
             for layer, message in zip(self.eq_layers, self.message_layers):
                 x = layer(message(x, mask), mask, nobj)
         return x
+        