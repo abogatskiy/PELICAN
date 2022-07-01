fix softmax

src/layers/perm_equiv_models.py
@@ -174,7 +174,7 @@ class Net2to2(nn.Module):
             for layer, message, sig, normlayer in zip(self.eq_layers, self.message_layers, self.attention, self.normlayers):
                 m = message(x, mask)        # form messages at each of the NxN nodes
                 y = sig(m)                  # compute the dot product with the attention vector over the channel dim
-                ms = self.softmax(y.view(B,-1), dim=-1).view_as(y) * mask
+                ms = torch.exp(self.softmax(y.view(B,-1), dim=-1)).view_as(y) * mask
                 ms = ms / ms.sum(dim=(1,2), keepdim=True)
                 # ms = y.sigmoid() * mask
                 z = normlayer(ms * m)       # apply LayerNorm, i.e. normalize over the channel dimension