5af89b623c169ee76a3fd47c684d70304a9f7cf0
level1807 <blackmail1807@gmail.com>
2022-07-04 10:33:51 -0400

    update seed fixing

--- src/trainer/utils.py
+++ src/trainer/utils.py
@@ -76,7 +76,11 @@ def fix_args(args):
         torch.manual_seed(seed)
     elif args.seed > 0:
         torch.manual_seed(args.seed)
-
+        torch.cuda.manual_seed(args.seed)
+        torch.cuda.manual_seed_all(args.seed)
+        torch.backends.cudnn.enabled = False
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
     return args
 
 def init_file_paths(args):
