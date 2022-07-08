2e59845b9e7ae281e16e24c21e172d6da077cdce
level1807 <blackmail1807@gmail.com>
2022-07-07 21:04:14 -0400

    optuna tweak for thin study

--- optuna_pelican_classifier.py
+++ optuna_pelican_classifier.py
@@ -32,20 +32,24 @@ def suggest_params(args, trial):
     # args.layernorm = trial.suggest_categorical("layernorm", [True, False])
     args.lr_decay_type = trial.suggest_categorical("lr_decay_type", ['exp', 'cos'])
 
-    args.batch_size = trial.suggest_categorical("batch_size", [32, 40])
+    args.batch_size = trial.suggest_categorical("batch_size", [32, 64])
 
     args.config = trial.suggest_categorical("config", ["s", "sm"]) # , "sM", "Sm"]) #, "S", "m", "M", "sS", "mM", "sM", "Sm", "SM"]) #, "mx", "Mx", "sSm", "sSM", "smM", "sMmM", "mxn", "mXN", "mxMX", "sXN", "smxn"])
     
-    n_layers1 = trial.suggest_int("n_layers1", 6, 8)
+    n_layers1 = trial.suggest_int("n_layers1", 4, 8)
 
-    # n_layersm = trial.suggest_int("n_layersm", 1, 2)
-    # args.num_channels_m = [[trial.suggest_int('n_channelsm['+str(k)+']', 10, 30) for k in range(n_layersm)]] * n_layers1
-    n_layersm = [trial.suggest_int("n_layersm", 1, 2) for i in range(n_layers1)]
-    args.num_channels_m = [[trial.suggest_int('n_channelsm['+str(i)+', '+str(k)+']', 10, 40) for k in range(n_layersm[i])] for i in range(n_layers1)]
+    # # n_layersm = trial.suggest_int("n_layersm", 1, 2)
+    # # args.num_channels_m = [[trial.suggest_int('n_channelsm['+str(k)+']', 10, 30) for k in range(n_layersm)]] * n_layers1
+    # n_layersm = [trial.suggest_int("n_layersm", 1, 2) for i in range(n_layers1)]
+    # args.num_channels_m = [[trial.suggest_int('n_channelsm['+str(i)+', '+str(k)+']', 10, 40) for k in range(n_layersm[i])] for i in range(n_layers1)]
 
-    args.num_channels1 = [trial.suggest_int("n_channels1["+str(i)+"]", 10, 40) for i in range(n_layers1 + 1)]
-    # args.num_channels1 = [trial.suggest_int("n_channels1", 3, 30)]
-    # args.num_channels1 = args.num_channels1 * (n_layers1) + [args.num_channels_m[0][0] if n_layersm > 0 else args.num_channels1[0]]
+    # args.num_channels1 = [trial.suggest_int("n_channels1["+str(i)+"]", 10, 40) for i in range(n_layers1 + 1)]
+    # # args.num_channels1 = [trial.suggest_int("n_channels1", 3, 30)]
+    # # args.num_channels1 = args.num_channels1 * (n_layers1) + [args.num_channels_m[0][0] if n_layersm > 0 else args.num_channels1[0]]
+
+    args.num_channels1 = [trial.suggest_int("n_channels1", 1, 10)] * n_layers1
+    args.num_channels_m = [[trial.suggest_int("n_channels1", 1, 10), args.num_channels1[0]*15*len(args.config)]] * n_layers1
+    args.num_channels1 = args.num_channels1 + [args.num_channels_m[0][0]]
 
     n_layers2 = trial.suggest_int("n_layers2", 1, 2)
     # n_layers2 = 1
@@ -184,47 +188,47 @@ if __name__ == '__main__':
     study = optuna.create_study(study_name=args.study_name, storage=storage, direction=direction, load_if_exists=True,
                                 pruner=pruner, sampler=sampler)
 
-    init_params =  {
-                    # 'activate_agg': False,
-                    # 'activate_lin': True,
-                    'activation': 'leakyrelu',
-                    'batch_size': 40,
-                    'config': 's',
-                    # 'lr_final': 1e-07,
-                    # 'lr_init': 0.001,
-                    # 'scale': 0.33,
-                    # 'num_epoch': 60,
-                    # 'sig': False,
-                    'n_channelsm[0, 0]': 25,
-                    'n_channelsm[0, 1]': 25,                    
-                    'n_channels1[0]': 25,
-                    'n_channelsm[1, 0]': 20,
-                    'n_channelsm[1, 1]': 20,
-                    'n_channels1[1]': 20,
-                    'n_channelsm[2, 0]': 15,
-                    'n_channelsm[2, 1]': 15,
-                    'n_channels1[2]': 15,
-                    'n_channelsm[3, 0]': 20,
-                    'n_channelsm[3, 1]': 20,
-                    'n_channels1[3]': 15,
-                    'n_channelsm[4, 0]': 25,
-                    'n_channelsm[4, 1]': 25,
-                    'n_channels1[4]': 20,
-                    'n_channels1[5]': 25,
-                    'n_channels2[0]': 25,
-                    # 'n_layers1': 5,
-                    # 'n_layers2': 1,
-                    'n_layersm[0]': 2,
-                    'n_layersm[1]': 2,
-                    'n_layersm[2]': 2,
-                    'n_layersm[3]': 2,
-                    'n_layersm[4]': 2,
-                    'n_layersm[5]': 2,
-                    # 'layernorm' : False,
-                    # 'drop_rate' : 0.15,
-                    # 'optim': 'adamw',
-                    }
-    study.enqueue_trial(init_params)
+    # init_params =  {
+    #                 # 'activate_agg': False,
+    #                 # 'activate_lin': True,
+    #                 'activation': 'leakyrelu',
+    #                 'batch_size': 40,
+    #                 'config': 's',
+    #                 # 'lr_final': 1e-07,
+    #                 # 'lr_init': 0.001,
+    #                 # 'scale': 0.33,
+    #                 # 'num_epoch': 60,
+    #                 # 'sig': False,
+    #                 'n_channelsm[0, 0]': 25,
+    #                 'n_channelsm[0, 1]': 25,                    
+    #                 'n_channels1[0]': 25,
+    #                 'n_channelsm[1, 0]': 20,
+    #                 'n_channelsm[1, 1]': 20,
+    #                 'n_channels1[1]': 20,
+    #                 'n_channelsm[2, 0]': 15,
+    #                 'n_channelsm[2, 1]': 15,
+    #                 'n_channels1[2]': 15,
+    #                 'n_channelsm[3, 0]': 20,
+    #                 'n_channelsm[3, 1]': 20,
+    #                 'n_channels1[3]': 15,
+    #                 'n_channelsm[4, 0]': 25,
+    #                 'n_channelsm[4, 1]': 25,
+    #                 'n_channels1[4]': 20,
+    #                 'n_channels1[5]': 25,
+    #                 'n_channels2[0]': 25,
+    #                 # 'n_layers1': 5,
+    #                 # 'n_layers2': 1,
+    #                 'n_layersm[0]': 2,
+    #                 'n_layersm[1]': 2,
+    #                 'n_layersm[2]': 2,
+    #                 'n_layersm[3]': 2,
+    #                 'n_layersm[4]': 2,
+    #                 'n_layersm[5]': 2,
+    #                 # 'layernorm' : False,
+    #                 # 'drop_rate' : 0.15,
+    #                 # 'optim': 'adamw',
+    #                 }
+    # study.enqueue_trial(init_params)
                             
     study.optimize(objective, n_trials=1, callbacks=[optuna.study.MaxTrialsCallback(200, states=(TrialState.COMPLETE,))])
 
