a4116bd4ac5b0a414155c2378dfebd8c3e0ff946
level1807 <blackmail1807@gmail.com>
2022-07-17 13:22:42 -0700

    tweak inputencoder init

--- src/layers/generic_layers.py
+++ src/layers/generic_layers.py
@@ -133,7 +133,7 @@ class InputEncoder(nn.Module):
         super().__init__()
 
         self.to(device=device, dtype=dtype)
-        self.alphas = nn.Parameter(1.05 * torch.rand(1, 1, 1, out_dim, device=device, dtype=dtype))
+        self.alphas = nn.Parameter(1.05 * torch.linspace(0, 1.05, out_dim, device=device, dtype=dtype).view(1, 1, 1, out_dim)) #torch.rand(1, 1, 1, out_dim, device=device, dtype=dtype))
         self.zero = torch.tensor(0, device=device, dtype=dtype)
 
     def forward(self, x, mask=None):
