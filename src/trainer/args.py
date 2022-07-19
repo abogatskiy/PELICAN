9d05100ddeb270f949386024af65510b3e2825d3
Alexander Bogatskii <abogatskii@gw162.flatironinstitute.org>
2022-07-19 02:43:45 -0400

    update defaults and slurm job

--- src/trainer/args.py
+++ src/trainer/args.py
@@ -170,12 +170,12 @@ def setup_argparse():
     parser.add_argument('--num-channels-m', nargs='*', type=int, metavar='N',
                         help='Number of channels to allow after mixing (default: [[40],[25],[10],[25],[30],[40]])',
                         # default=[[38], [24], [11], [24], [32], [40]]
-                        default = [[40],[25],[10],[25],[30],[40]]
+                        default = [[20],[20],[20],[20],[20],[20]]
                         )
     parser.add_argument('--num-channels1', nargs='*', type=int, metavar='N',
                         help='Number of channels to allow after mixing (default: [35,20,20,15,20,20,35])',
                         # default=[37,20,19,16,21,22,23]
-                        default=[35,20,20,15,20,20,35]
+                        default=[35,20,20,20,20,20,35]
                         )
     parser.add_argument('--num-channels2', nargs='*', type=int, default=[30], metavar='N',
                         help='Number of channels to allow after mixing (default: [30])')
