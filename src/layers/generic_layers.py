fix masking in c_safe_eq_layer, speed up IR/C safe PELICAN

src/layers/generic_layers.py
@@ -90,7 +90,7 @@ class MessageNet(nn.Module):
         activation_fn = get_activation_fn(activation)
         self.activations = nn.ModuleList([activation_fn for i in range(depth)])
         
-        if depth == 0: self.batchnorm = False
+        # if depth == 0: self.batchnorm = False
         if self.batchnorm == True:
             self.batchnorm = 'b'
         if self.batchnorm:
@@ -174,7 +174,7 @@ class InputEncoder(nn.Module):
         # x = ((self.betas.abs() + x).abs().pow(1e-6 + self.alphas ** 2) - self.betas.abs().pow(1e-6 + self.alphas ** 2)) / (1e-6 + self.alphas ** 2) #288
         # x = x * (x < (100 * self.betas.exp())) 
         # x = (1e-2 + x).abs().log()/2  # Add a logarithmic rescaling function before MLP to soften the heavy tails in inputs
-        
+
         if mode=='log':
             x = ((1 + x).abs().pow(1e-6 + self.alphas ** 2) - 1) / (1e-6 + self.alphas ** 2)
 
@@ -203,11 +203,11 @@ class SoftMask(nn.Module):
     def forward(self, x, mask=None, mode=''):
         
         if mode=='c':
-            masses = 100 * torch.diagonal(x, dim1=1, dim2=2)
+            masses = 100000 * torch.diagonal(x, dim1=1, dim2=2)
             x = torch.clamp(masses.unsqueeze(-1) * masses.unsqueeze(-2), min=-1., max=1.)
         
         if mode=='c1d':
-            masses = 100 * torch.diagonal(x, dim1=1, dim2=2)
+            masses = 100000 * torch.diagonal(x, dim1=1, dim2=2)
             x = torch.clamp(masses, min=-1., max=1.)
         
         if mode=='ir':

src/layers/perm_equiv_models.py
@@ -253,11 +253,29 @@ class Eq2to2(nn.Module):
             else:
                 raise ValueError("args.config must consist of the following letters: smxnSMXN", self.config)
             if softmask_ir is not None:
-                op = torch.cat([op[:,:,:3], op[:,:,3:] * softmask_ir], dim=2)
+                s = op.shape
+                softmask_ir = torch.cat([
+                                        torch.ones([s[0],1,3,s[-2],s[-1]], device=op.device), 
+                                        softmask_ir.expand([s[0],1,12,s[-2],s[-1]])
+                                        ], 2)
+                op = op*softmask_ir
             if softmask_c is not None:
-                op = torch.cat([op[:,:,:2] * softmask_c, op[:,:,2:5], op[:,:,5:12] * softmask_c, op[:,:,12:]], dim=2)
+                s = op.shape
+                softmask_c = torch.cat([
+                                        softmask_c.expand([s[0],1,2,s[-2],s[-1]]),
+                                        torch.ones([s[0],1,3,s[-2],s[-1]], device=op.device), 
+                                        softmask_c.expand([s[0],1,7,s[-2],s[-1]]),
+                                        torch.ones([s[0],1,3,s[-2],s[-1]], device=op.device)
+                                        ], 2)
+                op = op * softmask_c
             if softmask_irc is not None:
-                op = torch.cat([op[:,:,:2] * softmask_irc, op[:,:,[2]], op[:,:,3:] * softmask_irc], dim=2)
+                s = op.shape
+                softmask_irc = torch.cat([
+                                        softmask_irc.expand([s[0],1,2,s[-2],s[-1]]),
+                                        torch.ones([s[0],1,1,s[-2],s[-1]], device=op.device), 
+                                        softmask_irc.expand([s[0],1,12,s[-2],s[-1]]),
+                                        ], 2)
+                op = op * softmask_irc
             ops.append(op)
 
         ops = torch.cat(ops, dim=2)

src/models/pelican_cov.py
@@ -113,10 +113,10 @@ class PELICANRegression(nn.Module):
                 # This ensures that massless inputs survive in later layers only as part of sums over all momenta. 
                 # The original entries for those inputs get killed by the C-safe softmask. This is done only once, so the C-mask is not needed again.
                 # To avoid multiplying any entries by the mask twice, we use the softmask_irc option.
-                inputs = self.c_safe_eq_layer(inputs, softmask_irc=softmask_c.unsqueeze(1).unsqueeze(2))
+                inputs = self.c_safe_eq_layer(inputs, mask=edge_mask.unsqueeze(-1), softmask_irc=softmask_c.unsqueeze(1).unsqueeze(2))
             else:       
                 # In case of only C-safety, apply the same equivariant layer with the C-safe mask.  
-                inputs = self.c_safe_eq_layer(inputs, softmask_c=softmask_c.unsqueeze(1).unsqueeze(2))
+                inputs = self.c_safe_eq_layer(inputs, mask=edge_mask.unsqueeze(-1), softmask_c=softmask_c.unsqueeze(1).unsqueeze(2))
         elif self.ir_safe:
             # In case of only IR-safety, define a more lenient softmask that vanishes on a given
             #  row/column corresponding to an input particle p if p*J vanishes, where J is the total jet momentum.
@@ -150,8 +150,6 @@ class PELICANRegression(nn.Module):
         invariant_particle_coefficients =  self.mlp_out_1(act3, mask=particle_mask.unsqueeze(-1))
         prediction = (event_momenta.unsqueeze(-2) * invariant_particle_coefficients.unsqueeze(-1)).sum(1) / self.scale  # / nobj.squeeze(-1)
         prediction = prediction.squeeze(-2)
-
-        if prediction.isnan().any(): breakpoint()
 
         if covariance_test:
             return {'predict': prediction, 'weights': invariant_particle_coefficients}, [inputs, act1, act2, act3]