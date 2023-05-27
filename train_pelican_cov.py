7e6bf216824b8d960d1b2d1ebaea5a186d2b1238
abogatskiy <blackmail1807@gmail.com>
2023-05-27 11:43:20 -0400

    update readme

--- train_pelican_cov.py
+++ train_pelican_cov.py
@@ -101,7 +101,7 @@ def main():
 
 
     # Define a loss function. This is the loss function whose gradients are actually computed. 
-    loss_fn = lambda predict, targets:      0.01 * loss_fn_E(predict, targets) #0.05 * loss_fn_m(predict,targets) + 0.01 * loss_fn_3d(predict, targets) #+ 10 * loss_fn_psi(predict,targets) #  #    #+ 0.02 * loss_fn_E(predict,targets) #  #+  + 0.01 * loss_fn_pT(predict,targets) #  #0.03 * loss_fn_inv(predict,targets) + 
+    loss_fn = lambda predict, targets:      0.05 * loss_fn_m(predict,targets) + 0.01 * loss_fn_3d(predict, targets) #0.01 * loss_fn_E(predict, targets) #+ 10 * loss_fn_psi(predict,targets) #  #    #+ 0.02 * loss_fn_E(predict,targets) #  #+  + 0.01 * loss_fn_pT(predict,targets) #  #0.03 * loss_fn_inv(predict,targets) + 
     # loss_fn = lambda predict, targets:      0.0005 * loss_fn_col(predict,targets) + 0.01*(-predict[...,0]).relu().mean() + 0.001 * loss_fn_inv(predict,targets) # 0.1 * loss_fn_m(predict,targets)
 
     # Apply the covariance and permutation invariance tests.
