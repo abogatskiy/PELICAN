f38a097e5502b9a886897dc73cab2f3d5f75bbc2
abogatskiy <blackmail1807@gmail.com>
2023-09-04 00:14:49 -0400

    redefine multiclass confusion matrix

--- src/models/metrics_multiclass.py
+++ src/models/metrics_multiclass.py
@@ -3,11 +3,12 @@ import numpy as np
 from sklearn.metrics import confusion_matrix, roc_auc_score, roc_curve
 
 def metrics(predict, targets, loss_fn, prefix, logger=None):
+    num_classes = predict.shape[-1]
     loss = loss_fn(predict, targets.long()).item()
     accuracy = Accuracy(predict, targets).item()
     auc_score = AUCScore(predict, targets)
     roc, eB03s, eS03s, eB05s, eS05s = ROC(predict, targets)
-    conf_matrix = confusion_matrix(targets.argmax(dim=1), predict.argmax(dim=1)) / targets.shape[0]
+    conf_matrix = confusion_matrix(targets.argmax(dim=1), predict.argmax(dim=1)) / targets.shape[0] * num_classes
     metrics = {'loss': loss, 'accuracy': accuracy, 'AUC': auc_score, 'BgRejectionAt0.3': [1/eB03 if eB03>0 else 0 for eB03 in eB03s], 'atSignEfficiency03': eS03s, 'BgRejectionAt0.5': [1/eB05 if eB05>0 else 0 for eB05 in eB05s], 'atSignEfficiency05': eS05s, 'confusion': conf_matrix}
     string = ' L: {:10.4f}, ACC: {:10.4f}, AUC: {},    BR: {} @ {:>4.4f},    BR: {} @ {:>4.4f},\nconf:\n{}'.format(loss, accuracy, auc_score, [1/eB03 if eB03>0 else 0 for eB03 in eB03s], 0.3,  [1/eB05 if eB05>0 else 0 for eB05 in eB05s], 0.5, conf_matrix)
     np.savetxt(prefix+'_ROC.csv', np.concatenate(ROC(predict, targets)[0]), delimiter=',')
